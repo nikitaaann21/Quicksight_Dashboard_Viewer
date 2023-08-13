42a85e57676dc8dbf3a3c78f64b67fbcf3f8858a
nikitaaann21 <nikitaannmathen@gmail.com>
2023-08-13 23:07:28 +0530

    Edits made

--- dashboard/views.py
+++ dashboard/views.py
@@ -60,14 +60,16 @@ def signup_view(request):
         # Attach the required policy to the user
         policy_name = 'DashboardAccessPolicy'
         d_arn=f'arn:aws:quicksight:*:*:dashboard/{dashboard_name}'
-        policy_document = {
-            'Version': '2012-10-17',
-            'Statement': [{
-                'Effect': 'Allow',
-                'Action': 'quicksight:GetDashboardEmbedUrl',
-                'Resource': d_arn
-            }]
-        }
+        # policy_document = {
+        #     'Version': '2012-10-17',
+        #     'Statement': [{
+        #         'Effect': 'Allow',
+        #         'Action': 'quicksight:GetDashboardEmbedUrl',
+        #         'Resource': d_arn
+        #     }]
+        # }
+
+        policy_document="{'Version': '2012-10-17','Statement': {'Effect': 'Allow','Action': 'quicksight:GetDashboardEmbedUrl','Resource':"+ d_arn+"}}"
         iam_client.put_user_policy(UserName=uname, PolicyName=policy_name, PolicyDocument=policy_document)
 
 
